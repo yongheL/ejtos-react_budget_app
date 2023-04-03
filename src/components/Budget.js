903e1b6fabe90d8e8463071e1232607544542ec9
yongheL <114266112+yongheL@users.noreply.github.com>
2023-04-02 18:33:13 -0700

    Update Budget.js

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -1,10 +1,43 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import { AppContext } from '../context/AppContext';
+
+
 const Budget = () => {
-    const { budget } = useContext(AppContext);
+    const { budget, currency, expenses, dispatch } = useContext(AppContext);
+
+    const changeBudget = (val)=>{
+		const totalExpenses = expenses.reduce((total, item) => {
+			return (total += item.cost);
+		}, 0);
+
+		
+		if(val<totalExpenses) {
+			alert("You cannot reduce the budget value lower than the spending");
+		} else if (val > 20000) {
+            alert("You cannot exceed the budget limit of " + currency + "20,000");
+        } else {
+			dispatch({
+				type: 'SET_BUDGET',
+				payload: val,
+			})
+		}
+	}
+    
     return (
         <div className='alert alert-secondary'>
-            <span>Budget: £{budget}</span>
+            <span>Budget: {currency}
+            
+                <input 
+                    required='required'
+                    type='number'
+                    step='10'
+                    id='budget'
+                    value={budget}
+                    style={{ marginLeft: '2rem' , width: '100px'}}
+                    onInput={(event) => changeBudget(event.target.value)}>
+                </input>
+            
+            </span>
         </div>
     );
 };
