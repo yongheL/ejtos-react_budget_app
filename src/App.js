1dc8f485faac0617ce227d8026a143873a8916fc
yongheL <114266112+yongheL@users.noreply.github.com>
2023-04-02 18:30:06 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import { AppProvider } from './context/AppContext';
 import Remaining from './components/Remaining';
 import ExpenseTotal from './components/ExpenseTotal';
 import ExpenseList from './components/ExpenseList';
+import Currency from './components/Currency';
 import ExpenseItem from './components/ExpenseItem';
 import AllocationForm from './components/AllocationForm';
 
@@ -18,7 +19,7 @@ const App = () => {
         <AppProvider>
             <div className='container'>
                 <h1 className='mt-3'>Company's Budget Allocation</h1>
-                    <div className='row mt-3'>
+                    <div className='row mt-4'>
                         {/* Add Budget component here under */}        
                         <div className='col-sm'>
                             <Budget />
@@ -31,6 +32,10 @@ const App = () => {
                         <div className='col-sm'>
                             <ExpenseTotal />
                         </div> 
+
+                        <div className='col-sm'>
+                            <Currency />
+                        </div> 
                     </div>
                     <h3 className='mt-3'>Allocation</h3>
                     <div className='row '>  
@@ -41,7 +46,7 @@ const App = () => {
                     </div>
                         {/* Add ExpenseItem component here under */}        
                     
-                    <h3 className='mt-3'>ChangeAllocation</h3>
+                    <h3 className='mt-3'>Change Allocation</h3>
                     <div className='row mt-3'>
                         {/* Add AllocationForm component here under */}        
                         <div className='col-sm'>
